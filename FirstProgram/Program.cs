190836da6ec42b60e346ce8c8fad712ce91e6638
Panu Oksala <panu.oksala@adafy.com>
2022-04-07 09:38:03 +0300

    First example program

---
+++ FirstProgram/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+Console.WriteLine("Syötä kokonaisluku: ");
+var userInput = Console.ReadLine();
+
+if (int.TryParse(userInput, out int parsedNumber))
+{
+    var exponent = parsedNumber * parsedNumber;
+    Console.WriteLine(exponent);
+
+    if(exponent > 100)
+    {
+        Console.WriteLine("Sata");
+    }
+
+    if(exponent > 200)
+    {
+        Console.WriteLine("Kaksi sataa");
+    }
+}
+else
+{
+    Console.WriteLine("Syötetty arvo ei ollut kokonaisluku.");
+}
