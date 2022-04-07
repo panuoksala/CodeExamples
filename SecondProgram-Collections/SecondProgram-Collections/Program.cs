ccbc994364721b2cdfd5bf8cdcc6fb3696e9e999
Panu Oksala <panu.oksala@adafy.com>
2022-04-07 09:59:36 +0300

    Second program with method call and collections

---
+++ SecondProgram-Collections/SecondProgram-Collections/Program.cs
@@ -0,0 +1,27 @@
+﻿var listOfNumbers = new List<int> { 1, 5, 10, 20, 50 };
+
+int biggestFound = FindBiggestNumberFromCollection(listOfNumbers);
+
+Console.WriteLine(biggestFound);
+
+listOfNumbers.Add(7);
+listOfNumbers.Add(101);
+
+biggestFound = FindBiggestNumberFromCollection(listOfNumbers);
+
+Console.WriteLine(biggestFound);
+
+
+static int FindBiggestNumberFromCollection(List<int> listOfNumbers)
+{
+    var biggestFound = -1;
+    foreach (var number in listOfNumbers)
+    {
+        if (number > biggestFound)
+        {
+            biggestFound = number;
+        }
+    }
+
+    return biggestFound;
+}
