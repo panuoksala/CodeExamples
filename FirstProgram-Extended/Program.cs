affcdb0a425232365dba6c3de4f2ac0c7e5471df
Panu Oksala <panu.oksala@adafy.com>
2022-04-07 09:54:53 +0300

    Added extended version of first program

---
+++ FirstProgram-Extended/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+string userInput = "";
+do
+{
+    Console.WriteLine("Syötä desimaaliluku: ");
+    userInput = Console.ReadLine();
+
+    if (decimal.TryParse(userInput, out decimal parsedNumber))
+    {
+        var exponent = parsedNumber * parsedNumber;
+        // Use @ to escape characters in path.
+        File.AppendAllText(@"C:\temp\numbers.txt", exponent.ToString() + Environment.NewLine);
+
+        if (exponent > 100)
+        {
+            Console.WriteLine("Sata");
+        }
+
+        if (exponent > 200)
+        {
+            Console.WriteLine("Kaksi sataa");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Syötetty arvo ei ollut kokonaisluku.");
+    }
+} while (!string.Equals(userInput, "0"));
