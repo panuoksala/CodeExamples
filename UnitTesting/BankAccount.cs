559cc4c4321df46d8b24594795fa4c15ebf64edd
Panu Oksala <panu.oksala@adafy.com>
2022-04-26 11:59:05 +0300

    Added unit testing example

---
+++ UnitTesting/BankAccount.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+/// <summary>
+/// Copied from https://docs.microsoft.com/en-us/visualstudio/test/walkthrough-creating-and-running-unit-tests-for-managed-code?view=vs-2022
+/// </summary>
+namespace UnitTesting
+{
+    /// <summary>
+    /// Bank account demo class.
+    /// </summary>
+    public class BankAccount
+    {
+        private readonly string m_customerName;
+        private double m_balance;
+
+        private BankAccount() { }
+
+        public BankAccount(string customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+
+        public string CustomerName
+        {
+            get { return m_customerName; }
+        }
+
+        public double Balance
+        {
+            get { return m_balance; }
+        }
+
+        public void Debit(double amount)
+        {
+            if (amount > m_balance)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            m_balance += amount; // intentionally incorrect code
+        }
+
+        public void Credit(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            m_balance += amount;
+        }
+    }
+}
---
+++ UnitTesting/BankAccountTests.cs
@@ -0,0 +1,24 @@
+using Xunit;
+
+namespace UnitTesting
+{
+    public class BankAccountTests
+    {
+        [Fact]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            var account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // Act
+            account.Debit(debitAmount);
+
+            // Assert
+            double actual = account.Balance;
+            Assert.Equal(expected, actual, 3);
+        }
+    }
+}
