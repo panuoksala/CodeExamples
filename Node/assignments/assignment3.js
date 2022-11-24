8e498a82ce2b784aa450d69cdbc0db8dfa8a27a3
Panu Oksala <panu.oksala@adafy.com>
2022-11-24 15:25:53 +0200

    Removed old basics of programming folder

--- Node/assignments/assignment3.js
+++
@@ -1,6 +0,0 @@
-// Assignment 3
-function toSnakeCase(word, wordSecond) {
-    return word.replaceAll(" ", "_") + wordSecond.replaceAll(" ", "_");
-}
-
-console.log(toSnakeCase("This is test string", "And this is also"));
