0c0fc004c7a15c10195497d1b76c10208166c14e
Panu Oksala <panu.oksala@adafy.com>
2022-12-20 10:43:27 +0200

    Fixed assignment 8 to work with indecies correctly

--- Node/basics_of_programming/assignment8.js
+++ Node/basics_of_programming/assignment8.js
@@ -1,25 +1,27 @@
 // Find best buy and sell from given array of numbers
 function findBestTrades(rates) {
-    rates.sort((a,b) => a - b);
-    var bestBuy = rates[0];
-    var bestSell = rates[rates.length -1];
-    return {BestBuy: bestBuy, BestSell: bestSell};
+  const clone = [].concat(rates)
+
+  clone.sort((a,b) => a - b);
+  var bestBuy = clone[0];
+  var bestSell = clone[rates.length -1];
+  return {BestBuy: bestBuy, IndexBuy: rates.indexOf(bestBuy), BestSell: bestSell, IndexSell: rates.indexOf(bestSell)};
 }
-  
+
 // Build array of random numbers and return it.
 function getArrayOfRandomNumbers(amount) {
-  const randomNumbers = [];
-  for(var i=0;i<amount;i++)
-  {
-    // Append random number into array
-    randomNumbers.push(getRandomInt(301));
-  }
-  return randomNumbers;
+const randomNumbers = [];
+for(var i=0;i<amount;i++)
+{
+  // Append random number into array
+  varandomNumbers.push(getRandomInt(301));
+}
+return randomNumbers;
 }
 
 // JavaScript random returns float, so we will use floor to get the integer version.
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max);
 }
 
 // Get 5 random numbers
@@ -28,4 +30,3 @@ console.log(numbers);
 // Get best trade for these random numbers
 var result = findBestTrades(numbers);
 console.log(result);
-  
